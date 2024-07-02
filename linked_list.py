97bba6e33c614cd7e0d0d73c334b319922614b29
maggieshelt0n <maggiekshelton@gmail.com>
2024-07-02 09:08:42 -0400

    Completed linked_list.py
    finished but needs to be checked for bugs

--- linked_list.py
+++ linked_list.py
@@ -8,3 +8,63 @@ class Node:
 
     def __init__(self, data):
         self.data = data
+
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+    
+class LinkedList:
+    """Singly linked list
+    """
+  
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+    
+    def size(self):
+        """Returns the number of nodes in the list
+        Takes O(n) time
+        """
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count
+    
+    def add(self, data):
+        """Adds a new Node containing data at the head of the list
+        Takes O(1) time --> best case scenario
+        """
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def __repr__(self):
+        """
+        Return a string representation of the list.
+        Takes O(n) time.
+        """
+        nodes = []
+        current = self.head
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+            current = current.next_node
+        return  '-> '.join(nodes)
+
+
+
+
+
+
+
+
